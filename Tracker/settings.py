c34317cd1fd7c100b1a78de70f9427714df6e8b0
rittz1998 <riddhir158@gmail.com>
2023-11-13 19:14:44 -0600

    committing the file

--- Tracker/settings.py
+++ Tracker/settings.py
@@ -25,9 +25,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-9xbfyog&@wndd(z0_lp!&u+$+sddc+dt2j^6*&@!780wtt$r()'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['https://peaceful-waters-10658-52f8f7382eec.herokuapp.com/']
 
 
 # Application definition
