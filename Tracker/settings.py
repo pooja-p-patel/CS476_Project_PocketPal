1da7e18b077c51d1b9d98f0ca3d278452b0a63c8
rittz1998 <riddhir158@gmail.com>
2023-11-13 23:31:37 -0600

    added settings

--- Tracker/settings.py
+++ Tracker/settings.py
@@ -128,7 +128,7 @@ USE_TZ = True
 STORAGES = {
     # ...
     "staticfiles": {
-        "BACKEND": "whitenoise.storage.CompressedManifestStaticFilesStorage",
+        "BACKEND": "whitenoise.storage.CompressedStaticFilesStorage",
     },
 }
 
@@ -136,7 +136,8 @@ STATIC_URL = 'static/'
 STATICFILES_DIRS = [os.path.join(BASE_DIR,'static')]
 STATIC_ROOT = os.path.join(BASE_DIR,'staticfiles')
 # STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
-# STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+# STATICFILES_STORAGE = '"whitenoise.storage.CompressedStaticFilesStorage"'
+
 
 
 
